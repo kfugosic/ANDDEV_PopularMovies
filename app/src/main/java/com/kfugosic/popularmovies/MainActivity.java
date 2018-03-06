48b36582f4fbe3c58c47fc3b41c8330c8c720ca5
Kristijan Fugošić <kfugosic@gmail.com>
2018-03-07 00:31:53 +0100

    Implemented Loader instead of AsyncTask

--- app/src/main/java/com/kfugosic/popularmovies/MainActivity.java
+++ app/src/main/java/com/kfugosic/popularmovies/MainActivity.java
@@ -3,28 +3,38 @@ package com.kfugosic.popularmovies;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.PersistableBundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.kfugosic.popularmovies.models.Movie;
+import com.kfugosic.popularmovies.utils.JsonUtils;
 import com.kfugosic.popularmovies.utils.NetworkUtils;
 import com.kfugosic.popularmovies.enums.SortType;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements ListItemClickListener {
+public class MainActivity extends AppCompatActivity implements ListItemClickListener, LoaderManager.LoaderCallbacks<String> {
 
-    private static final int NUMBER_OF_COLUMNS_PORTRAIT = 2;
-    private static final int NUMBER_OF_COLUMNS_LANDSCAPE = 3;
-    private static final int LANDSCAPE_ORIENTATION_ID = 2;
+    private static final int MOVIEDB_QUERY_LOADER_HIGHESTRATED = 1;
+    private static final int MOVIEDB_QUERY_LOADER_POPULAR = 2;
+
+    private static final String MOVIEDB_QUERY_URL_EXTRA = "query_url";
     private static final String SORT_TYPE = "sort_type";
-
     private static final SortType DEFAULT_SORT_TYPE = SortType.POPULAR;
 
     private SortType currentSortType;
@@ -85,16 +95,94 @@ public class MainActivity extends AppCompatActivity implements ListItemClickList
         return super.onOptionsItemSelected(item);
     }
 
-    private void fillAdapter() {
-        URL queryUrl = NetworkUtils.buildUrl(currentSortType);
-        new MovieDbQueryTask(moviesGrid, this, this).execute(queryUrl);
-    }
-
     @Override
     public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
         outState.putSerializable(SORT_TYPE, currentSortType);
         super.onSaveInstanceState(outState, outPersistentState);
     }
+
+    //
+    // Loading movies grid
+    //
+
+    private void fillAdapter() {
+        URL queryUrl = NetworkUtils.buildUrl(currentSortType);
+        Bundle queryBundle = new Bundle();
+        queryBundle.putString(MOVIEDB_QUERY_URL_EXTRA, queryUrl.toString());
+
+        int loaderId = currentSortType == SortType.POPULAR ? MOVIEDB_QUERY_LOADER_POPULAR : MOVIEDB_QUERY_LOADER_HIGHESTRATED;
+        LoaderManager loaderManager = getSupportLoaderManager();
+        Loader<String> moviesGridLoader = loaderManager.getLoader(loaderId);
+
+        if (moviesGridLoader == null) {
+            loaderManager.initLoader(loaderId, queryBundle, this);
+        } else {
+            loaderManager.restartLoader(loaderId, queryBundle, this);
+        }
+    }
+
+    @Override
+    public Loader<String> onCreateLoader(int id, final Bundle args) {
+        return new AsyncTaskLoader<String>(this) {
+
+            String mQueryResultJson;
+
+            @Override
+            protected void onStartLoading() {
+                super.onStartLoading();
+                if(args==null){
+                    return;
+                }
+                if (mQueryResultJson != null) {
+                    deliverResult(mQueryResultJson);
+                } else {
+                    forceLoad();
+                }
+            }
+
+            @Override
+            public String loadInBackground() {
+                String queryUrl = args.getString(MOVIEDB_QUERY_URL_EXTRA);
+                if (queryUrl == null || queryUrl.isEmpty()) {
+                    return null;
+                }
+                String queryResult = null;
+                try {
+                    queryResult = NetworkUtils.getResponseFromHttpUrl(new URL(queryUrl));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return queryResult;
+            }
+
+            @Override
+            public void deliverResult(String data) {
+                mQueryResultJson = data;
+                super.deliverResult(data);
+            }
+
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+        if (data != null && !data.isEmpty()) {
+            List<Movie> allMovies = JsonUtils.parseMovieJson(data);
+            MovieAdapter adapter = new MovieAdapter(allMovies, this);
+            moviesGrid.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+        } else {
+            Toast.makeText(this, R.string.error_text, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+    }
+
+    //
+    // Utils
+    //
 
     private int numberOfColumns() {
         DisplayMetrics displayMetrics = new DisplayMetrics();
--- app/src/main/java/com/kfugosic/popularmovies/utils/JsonUtils.java
+++ app/src/main/java/com/kfugosic/popularmovies/utils/JsonUtils.java
@@ -1,8 +1,49 @@
 package com.kfugosic.popularmovies.utils;
 
+import com.kfugosic.popularmovies.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Kristijan on 06-Mar-18.
  */
 
 public class JsonUtils {
+
+    private static final String JSON_ALL_RESULTS = "results";
+    private static final String JSON_MOVIE_ID = "id";
+    private static final String JSON_MOVIE_TITLE = "original_title";
+    private static final String JSON_MOVIE_OVERVIEW = "overview";
+    private static final String JSON_POSTER_PATH = "poster_path";
+    private static final String JSON_USER_RATING = "vote_average";
+    private static final String JSON_RELEASE_DATE = "release_date";
+
+    public static List<Movie> parseMovieJson(String json) {
+        List<Movie> movies = new ArrayList<>();
+        try {
+            JSONObject moviesJson = new JSONObject(json);
+            JSONArray allMovies = moviesJson.getJSONArray(JSON_ALL_RESULTS);
+            for (int i = 0; i < allMovies.length(); i++) {
+                JSONObject currentMovie = allMovies.getJSONObject(i);
+                Movie newMovie = new Movie(
+                        currentMovie.optString(JSON_MOVIE_ID),
+                        currentMovie.optString(JSON_MOVIE_TITLE),
+                        currentMovie.optString(JSON_MOVIE_OVERVIEW),
+                        currentMovie.optString(JSON_POSTER_PATH),
+                        currentMovie.optString(JSON_USER_RATING),
+                        currentMovie.optString(JSON_RELEASE_DATE)
+                );
+                movies.add(newMovie);
+            }
+        } catch (JSONException e) {
+            movies = null;
+            e.printStackTrace();
+        }
+        return movies;
+    }
 }
